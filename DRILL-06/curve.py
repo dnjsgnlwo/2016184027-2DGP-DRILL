37d6b51d3dee7536529a47eea74b6467cb11b8f8
Malonian <wonwhijae20@gmail.com>
2019-10-11 14:24:24 +0900

    DRILL-06 실습2번 draw character, kpu

--- DRILL-06/curve.py
+++ DRILL-06/curve.py
@@ -50,19 +50,6 @@ def draw_point(p):
     turtle.dot(5, random.random(), random.random(), random.random())
 
 
-def draw_curve_3_points(p1, p2, p3):
-    draw_big_point(p1)
-    draw_big_point(p2)
-    draw_big_point(p3)
-
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
-        draw_point((x, y))
-    draw_point(p3)
-
-
 def draw_curve_4_points(p1, p2, p3, p4):
     draw_big_point(p1)
     draw_big_point(p2)
@@ -81,9 +68,9 @@ def draw_curve_4_points(p1, p2, p3, p4):
     for i in range(0, 100, 2):
         t = i / 100
         x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+                -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+                -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
         draw_point((x, y))
     draw_point(p3)
 
@@ -98,15 +85,15 @@ def draw_curve_4_points(p1, p2, p3, p4):
     for i in range(0, 100, 2):
         t = i / 100
         x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
+                -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
         y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
+                -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
         draw_point((x, y))
     draw_point(p1)
 
 
 prepare_turtle_canvas()
-
-draw_curve_4_points((-300, 200), (400, 350), (300, -300), (-200, -200))
+while True:
+    draw_curve_4_points((-300, 200), (400, 350), (300, -300), (-200, -200))
 
 turtle.done()
